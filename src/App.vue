1ea7808a1f334600a1fe3bffad82a71fa687ae9d
Michele Abategiovanni <112349713+MicheleAbategiovanni@users.noreply.github.com>
2023-02-16 15:38:27 +0100

    add structure card project

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,50 @@
 <template>
   <TheHeader></TheHeader>
 
-<h1 class="bg-danger">Hello</h1>
+  <h1 class="bg-danger">Hello</h1>
+
+  <main>
+
+    <div class="container">
+
+      <ProjectCard v-for="(project, index) in projects" />
+
+    </div>
+
+</main>
 </template>
 
+
 <script>
+import axios from "axios";
+
 import TheHeader from "./components/TheHeader.vue";
+import ProjectCard from "./components/ProjectCard.vue";
 
 export default {
-  components: { TheHeader },
+  components: { TheHeader, ProjectCard },
   data() {
     return {
+      backendUrl: "http://localhost:8000",
+      projects: [],
+    };
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(this.backendUrl + "/api/projects").then((resp) => {
 
+        this.projects = resp.data;
+
+        console.log(this.projects);
+
+      });
     }
-  }
-
+  },
+  mounted() {
+    this.fetchProjects();
+  },
 }
+
 </script>
 
 
