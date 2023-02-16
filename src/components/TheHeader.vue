bebf3c389e18b08c4b4b817871d3fe47c739cc56
Michele Abategiovanni <112349713+MicheleAbategiovanni@users.noreply.github.com>
2023-02-16 14:24:01 +0100

    complete structure header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,64 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark">
+
+            <div class="container">
+
+                <div class="d-flex">
+                    <a class="navbar-brand text-white" href="#">LOGO</a>
+                </div>
+
+                <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation" @click="onClickHamburger">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
+
+                    <div class="row p-2 align-items-center">
+
+                        <div class="col">
+                            <ul class="navbar-nav  mb-2 mb-lg-0 ">
+
+                                <li class="nav-item" v-for="(item, index) in items" :key="index" >
+                                    <a class="nav-link text-white" href="#">{{item.label}}</a>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </nav>
+    </header>
+</template>
+  
+<script>
+export default {
+    name: "TheHeader",
+    data() {
+        return {
+            items: [
+                {
+                    label: "Home",
+                    routeName: "home",
+                },
+                {
+                    label: "Progetti",
+                    routeName: "projects.index",
+                },
+            ],
+        };
+    },
+};
+</script>
+  
+<style scoped lang="scss">
+
+
+</style>
